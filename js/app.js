9bdfda72c3460b602b665b73d6f1b67063d88d8f
reniatt <renatapyttel@gmail.com>
2017-07-30 20:23:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,12 +26,12 @@ var main = function() {
   var images = [];
   var time = 4000;
 
-  images[0] = 'homberg-1959229__340.jpg';
-  images[1] = 'bled-1899264__340.jpg';
-  images[2] = 'clouds-2517653_1920.jpg';
-  images[3] = 'big-waves-2193828_1920.jpg';
-  images[4] = 'suns-rays-478249_1920.jpg';
-  images[5] = 'lebanon-2542916_1920.jpg';
+  images[0] = 'image/homberg-1959229__340.jpg';
+  images[1] = 'image/bled-1899264__340.jpg';
+  images[2] = 'image/clouds-2517653_1920.jpg';
+  images[3] = 'image/big-waves-2193828_1920.jpg';
+  images[4] = 'image/suns-rays-478249_1920.jpg';
+  images[5] = 'image/lebanon-2542916_1920.jpg';
   
   function changeImg() {
   document.slide.src = images[i];
